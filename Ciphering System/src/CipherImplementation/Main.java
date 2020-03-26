96c8ee13a3c358c660454678a67d693606f37b09
Ziad Moftah <ziadh434@gmail.com>
2020-03-26 21:19:04 +0200

    Moved some functions to the vigenere abstract class
    - Helper functions ( islower, isupper, isalpha)
    - DecryptChar
    -EncryptChar

--- Ciphering System/src/CipherImplementation/Main.java
+++ Ciphering System/src/CipherImplementation/Main.java
@@ -3,8 +3,8 @@ package CipherImplementation;
 
 public class Main {
     public static void main(String[] args) {
-        VigenereWithReapeatingKey VG= new VigenereWithReapeatingKey("MICHIGAN TECHNOLOGICAL UNIVERSITY"
-                , "HOUGHTON") ;
+        VigenereWithReapeatingKey VG= new VigenereWithReapeatingKey("A"
+                , "B") ;
         System.out.println(VG.Encrypt()) ;
         System.out.println(VG.Decrypt());
 
--- Ciphering System/src/CipherImplementation/VigenereWithReapeatingKey.java
+++ Ciphering System/src/CipherImplementation/VigenereWithReapeatingKey.java
@@ -22,51 +22,7 @@ public class VigenereWithReapeatingKey extends  Vigenere{
         return EncryptedText;
     }
 
-    private Boolean IsAlpha(char in){
-        return ( in >= 'A' &&  in <= 'Z') || ( in >= 'a' && in <= 'z') ;
-    }
-    private Boolean IsLowerCase( char in ){
-        return in >= 'a' && in <= 'z' ;
-    }
-    private Boolean IsUpperCase( char in ){
-        return in >= 'A' && in <= 'Z' ;
-    }
-    private char EncryptChar ( char in , char currKey ){
-        if ( IsAlpha(in)){
-            if ( IsLowerCase(in)){
-                int x = (in + Character.toLowerCase(currKey)) % 26 ;
-                x += 'a' ;
-                return (char)(x) ;
-            }
-            else if ( IsUpperCase(in)){
-                int x = (in + Character.toUpperCase(currKey)) % 26 ;
-                x += 'A' ;
-                return (char)(x) ;
-            }
-        }
-        else {
-            return in ;
-        }
-        return 0 ;
-    }
-    private char DecryptChar ( char in , char currKey ){
-        if ( IsAlpha(in)){
-            if ( IsLowerCase(in)){
-                int x = (in - Character.toLowerCase(currKey)+26) % 26 ;
-                x += 'a' ;
-                return (char)(x) ;
-            }
-            else if ( IsUpperCase(in)){
-                int x = (in - Character.toUpperCase(currKey)+26) % 26 ;
-                x += 'A' ;
-                return (char)(x) ;
-            }
-        }
-        else {
-            return in ;
-        }
-        return 0 ;
-    }
+
     @Override
     public String Decrypt() {
         char plaintext[] = new char[EncryptedText.length()] ;
