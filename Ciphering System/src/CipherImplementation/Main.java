6b869128253b5099c3232fddbf16f7926f5004ab
Ziad Moftah <ziadh434@gmail.com>
2020-03-26 18:21:58 +0200

    Added functions in the Vignere with repeating key
    - decrypt()
    - Encrypt()
    - GetNewKey()
    - Private Helper funtions (isaplha , islower , isupper)

--- Ciphering System/src/CipherImplementation/Main.java
+++ Ciphering System/src/CipherImplementation/Main.java
@@ -3,7 +3,10 @@ package CipherImplementation;
 
 public class Main {
     public static void main(String[] args) {
-        VigenereWithReapeatingKey VG= new VigenereWithReapeatingKey("Ziad Hassan", "dip") ;
+        VigenereWithReapeatingKey VG= new VigenereWithReapeatingKey("MICHIGAN TECHNOLOGICAL UNIVERSITY"
+                , "HOUGHTON") ;
+        System.out.println(VG.Encrypt()) ;
+        System.out.println(VG.Decrypt());
 
     }
 
--- Ciphering System/src/CipherImplementation/VigenereWithReapeatingKey.java
+++ Ciphering System/src/CipherImplementation/VigenereWithReapeatingKey.java
@@ -1,5 +1,6 @@
 package CipherImplementation;
 
+
 public class VigenereWithReapeatingKey extends  Vigenere{
     public VigenereWithReapeatingKey(String plainText, String key) {
         super(plainText, key);
@@ -13,25 +14,79 @@ public class VigenereWithReapeatingKey extends  Vigenere{
 
     @Override
     public String Encrypt() {
-        return null;
+        char encryptedtext[] = new char[PlainText.length()] ;
+        for ( int i = 0 ; i < PlainText.length() ; i ++ ){
+            encryptedtext[i] = EncryptChar(PlainText.charAt(i) , NewKey.charAt(i)) ;
+        }
+        EncryptedText = new String(encryptedtext) ;
+        return EncryptedText;
     }
 
+    private Boolean IsAlpha(char in){
+        return ( in >= 'A' &&  in <= 'Z') || ( in >= 'a' && in <= 'z') ;
+    }
+    private Boolean IsLowerCase( char in ){
+        return in >= 'a' && in <= 'z' ;
+    }
+    private Boolean IsUpperCase( char in ){
+        return in >= 'A' && in <= 'Z' ;
+    }
+    private char EncryptChar ( char in , char currKey ){
+        if ( IsAlpha(in)){
+            if ( IsLowerCase(in)){
+                int x = (in + Character.toLowerCase(currKey)) % 26 ;
+                x += 'a' ;
+                return (char)(x) ;
+            }
+            else if ( IsUpperCase(in)){
+                int x = (in + Character.toUpperCase(currKey)) % 26 ;
+                x += 'A' ;
+                return (char)(x) ;
+            }
+        }
+        else {
+            return in ;
+        }
+        return 0 ;
+    }
+    private char DecryptChar ( char in , char currKey ){
+        if ( IsAlpha(in)){
+            if ( IsLowerCase(in)){
+                int x = (in - Character.toLowerCase(currKey)+26) % 26 ;
+                x += 'a' ;
+                return (char)(x) ;
+            }
+            else if ( IsUpperCase(in)){
+                int x = (in - Character.toUpperCase(currKey)+26) % 26 ;
+                x += 'A' ;
+                return (char)(x) ;
+            }
+        }
+        else {
+            return in ;
+        }
+        return 0 ;
+    }
     @Override
     public String Decrypt() {
-        return null;
+        char plaintext[] = new char[EncryptedText.length()] ;
+        for ( int i = 0 ; i < PlainText.length() ; i ++ ){
+            plaintext[i] = DecryptChar(EncryptedText.charAt(i) , NewKey.charAt(i) ) ;
+        }
+        PlainText = new String(plaintext) ;
+        return PlainText;
     }
 
     @Override
     String GetNewKey(){
-        int SpacesCount_In_PlainText = 0;
-        for ( int i = 0 ; i < PlainText.length() ; i ++){
-            if ( PlainText.charAt(i) == ' '){
-                SpacesCount_In_PlainText ++  ;
+        int count = 0 ;
+        char[] newkey = new char[PlainText.length()] ;
+        for ( int i = 0 ; i < PlainText.length(); i ++){
+            if ( PlainText.charAt(i) == ' ' ){
+                newkey[i] = ' ' ;
+                continue;
             }
-        }
-        char[] newkey = new char[PlainText.length()-SpacesCount_In_PlainText] ;
-        for ( int i = 0 ; i < PlainText.length()-SpacesCount_In_PlainText; i ++){
-            newkey[i] = Key.charAt(i%(Key.length())) ;
+            newkey[i] = Key.charAt((count++)%(Key.length())) ;
         }
         NewKey = new String ( newkey ) ;
         return NewKey ;
