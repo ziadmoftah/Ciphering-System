9592480e5a1467f235b142bcd9634d3d028a7cee
Ziad Moftah <ziadh434@gmail.com>
2020-05-03 00:16:18 +0200

    Created a new Package For File
    Created a class which contains two static function one to save to a selected destination and the other to load from a certain destination

---
+++ Ciphering System/src/File/FileInputOutput.java
@@ -0,0 +1,29 @@
+package File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.nio.file.*;
+
+public class FileInputOutput {
+    static String Data ;
+    static FileWriter FW ;
+    public static Boolean SaveToFile(String Filename , String to_be_saved){
+        try{
+            FW = new FileWriter(Filename) ;
+            FW.write(to_be_saved) ;
+            FW.close();
+            return true ;
+        }
+        catch ( Exception e ){
+            return false ;
+        }
+    }
+    public static String LoadFromFile(String fileName) {
+        try{
+            Data = new String(Files.readAllBytes(Paths.get(fileName)));
+            return Data;
+        }
+        catch (Exception e ){
+            return new String ( "File Doesn't exist Please check the file name");
+        }
+    }
+}
